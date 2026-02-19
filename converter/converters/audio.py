a444e3b495595a87f51f06ae2892366482470fe4
rajsrivastav344 <srivastavraj344@gmail.com>
2026-02-19 13:39:15 +0530

    Added homepage screenshot

--- converter/converters/audio.py
+++ converter/converters/audio.py
@@ -1,9 +1,13 @@
 import subprocess
 import os
 
-def convert_audio(input_file, output_format):
-    output_file = os.path.splitext(input_file)[0] + "." + output_format
-    subprocess.run([
-        "ffmpeg", "-y", "-i", input_file, output_file
-    ], check=True)
+def convert_audio(input_file, output_format, output_dir):
+    base = os.path.splitext(os.path.basename(input_file))[0]
+    output_file = os.path.join(output_dir, base + "." + output_format)
+
+    subprocess.run(
+        ["ffmpeg", "-y", "-i", input_file, output_file],
+        check=True
+    )
+
     return output_file
